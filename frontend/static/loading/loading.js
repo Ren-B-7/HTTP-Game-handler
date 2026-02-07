eb1f5b41512eb0491047080710657e25446f9a3f
Ren B <Renier52147@gmail.com>
2026-02-07 15:25:56 +0200

    Delete frontend directory

--- frontend/static/loading/loading.js
+++
@@ -1,9 +0,0 @@
-// Show loading screen when navigating
-window.addEventListener("beforeunload", function async() {
-  document.getElementById("loading").style.display = "flex";
-});
-
-// Hide loading screen once the page loads
-window.addEventListener("load", function async() {
-  document.getElementById("loading").style.display = "none";
-});

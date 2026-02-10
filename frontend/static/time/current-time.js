90d2503c7c8fc1620993477fb47174914e162f76
Ren B <Renier52147@gmail.com>
2026-02-10 19:36:20 +0200

    Delete frontend directory

--- frontend/static/time/current-time.js
+++
@@ -1,9 +0,0 @@
-function updateTime() {
-  const el = document.getElementById("current-time");
-  if (!el) return;
-
-  el.textContent = new Date().toLocaleTimeString();
-}
-
-updateTime(); // run once
-setInterval(updateTime, 1000);
